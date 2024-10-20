88203d7441c5aaad8f451adab7057193bbb19f1b
Yutosaki <sasayu8190@gmail.com>
2024-10-20 15:19:29 +0900

    refactor: apply formatter

--- easy/merge_sorted_array.go
+++ easy/merge_sorted_array.go
@@ -5,22 +5,22 @@ import (
 	"sort"
 )
 
-func merge(nums1 []int, m int, nums2 []int, n int)  {
-    j:=0
-    for i:=m;i<len(nums1);i++{
-        nums1[i]=nums2[j]
-        j++
-    }
-    sort.Ints(nums1)
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	j := 0
+	for i := m; i < len(nums1); i++ {
+		nums1[i] = nums2[j]
+		j++
+	}
+	sort.Ints(nums1)
 }
 
 func main() {
 	// Test cases to validate the solution
 	// Test case 1
 	// Expected output: [1,2,2,3,5,6]
-	nums1 := []int{1,2,3,0,0,0}
+	nums1 := []int{1, 2, 3, 0, 0, 0}
 	m := 3
-	nums2 := []int{2,5,6}
+	nums2 := []int{2, 5, 6}
 	n := 3
 	merge(nums1, m, nums2, n)
 
