f36696e9fd6a36fb79d6bb5952f7b236c3986cc7
Yutosaki <sasayu8190@gmail.com>
2024-10-20 14:06:42 +0900

    feat: bad question, you just have to return arrays without val

---
+++ easy/remove_element.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+    counter:=0
+    for i:=0;i<len(nums);i++{
+        if nums[i] != val{
+            nums[counter] = nums[i]
+            counter++
+        }
+    }
+	nums = nums[:counter]
+	fmt.Println(nums)
+    return counter
+}
+
+func main(){
+	// Test cases to validate the solution
+	// Test case 1
+	// Expected output: 5
+	nums := []int{0,1,2,2,3,0,4,2}
+	val := 2
+	fmt.Println(removeElement(nums, val))
+	fmt.Println(nums)
+}
