3d0644ad968d1f77d2bffb4ee359da4d637f6886
Yutosaki <sasayu8190@gmail.com>
2024-10-23 18:38:35 +0900

    reafator: apply format

--- medium/remove_duplicates_from_sorted_array_2.go
+++ medium/remove_duplicates_from_sorted_array_2.go
@@ -7,19 +7,19 @@ func removeDuplicates(nums []int) int {
 		return len(nums)
 	}
 
-    k:=2
-    for i:=2;i<len(nums);i++{
-        if nums[i] != nums[k-2]{
-           nums[k] = nums[i]
-           k++
-        }
-    }
-    return k
+	k := 2
+	for i := 2; i < len(nums); i++ {
+		if nums[i] != nums[k-2] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
 }
 
 func main() {
-    nums := []int{1, 1, 2, 2, 2, 3}
-    newLength := removeDuplicates(nums)
-    fmt.Println("New length:", newLength)
-    fmt.Println("Modified array:", nums[:newLength])
+	nums := []int{1, 1, 2, 2, 2, 3}
+	newLength := removeDuplicates(nums)
+	fmt.Println("New length:", newLength)
+	fmt.Println("Modified array:", nums[:newLength])
 }
